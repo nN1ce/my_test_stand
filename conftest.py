1786ca068db23d442a912f3a750b658d8a7c8564
nN1ce <nicevector@yandex.ru>
2022-09-04 12:15:44 +0300

    в requirements.txt <- pytest second_test_use_fixture.py повтор второго урока (фикстуры + тест на поиск текста на сайте + негативный тест)

---
+++ conftest.py
@@ -0,0 +1,12 @@
+#conftest.py используется для хранения общих фикстур, которые используются для всего проетка
+
+from selene.support.shared import browser
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def seting_window_browser():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    yield
+    browser.close()
---
+++ second_test_use_fixture.py
@@ -0,0 +1,14 @@
+from selene.support.shared import browser
+from selene import have
+import pytest
+
+def test_search_text_in_google(seting_window_browser):
+    browser.open('https://google.com/ncr')
+    browser.element('[name=q]').type('selene python').press_enter()
+    browser.element('[class=v7W49e]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_2_search_text_in_google(seting_window_browser):
+    browser.open('https://google.com/ncr')
+    browser.element('[name=q]').type('selene python').press_enter()
+    browser.element('[class=v7W49e]').should(have.text('C++ - User-oriented Web UI browser tests in Python'))
