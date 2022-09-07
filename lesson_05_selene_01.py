a1bcef509b35e67286f4efea72145b0c29223a06
nN1ce <nicevector@yandex.ru>
2022-09-07 21:52:22 +0300

    add lesson05 Selene#1

---
+++ lesson_05_selene_01.py
@@ -0,0 +1,15 @@
+from selene.support.shared import browser
+from selene import have, be
+
+def test_submit_user_details():
+    browser.config.timeout = 3
+    browser.config.base_url = 'https://demoqa.com'
+
+    browser.open('https://demoqa.com/text-box')
+    browser.should(have.title('ToolsQA'))
+    browser.element('.main-header').should(have.text('Text Box'))
+    browser.all('#userFrom input, #userForm textarea').should(have.size(2))
+
+    browser.element('#userName').type('Evgen')
+    browser.element('#submit').press_enter()
+    browser.close()
